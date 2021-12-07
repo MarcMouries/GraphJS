040fb29bbd413113c79368ac3393d498653a8420
Marc Mouries <marc@mouries.net>
2021-12-07 08:23:24 -0500

    test for this.options.display_grid

--- src/index.old.js
+++
@@ -1,9 +0,0 @@
-
-export { default as hello } from "./hello";
-export * from "./shape";
-export {MChart} from "./MChart";
-
-export var version = "0.1"
-
-
-export { Circle, Rectangle, hello, version };
