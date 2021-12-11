f5362b9e45cac52616c3be04992cfa8b2aea78b7
Marc Mouries <marc@mouries.net>
2021-12-11 17:37:02 -0500

    new

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,3 +1,4 @@
+//export default function hello(name) {
 function hello(name) {
     return ("hello " + name) ;
   }
@@ -516,7 +517,46 @@ function rectContainsCircle(rectangle, circle) {
   return true;
 }
 
+/*
+2D vector implementation.
+Based on the vector functions in P5.js 
+*/
+class Vector {
+	constructor(x, y) {
+		this.x = x || 0;
+		this.y = y || 0;
+	}
+
+	static add(v1, v2) {
+		return new Vector(v1.x + v2.x, v1.y + v2.y);
+	}
+	static lerp(v1, v2, amount) {
+		let result = v1.copy();
+		return result.lerp(v2, amount);
+	}
+
+	lerp(v1, amount) {
+		this.x += (v1.x - this.x) * amount || 0;
+		this.y += (v1.y - this.y) * amount || 0;
+		return this;
+	}
+	copy() {
+		return new Vector(this.x, this.y);
+	}
+
+	sub(v) {
+		this.x -= v.x || 0;
+		this.y -= v.y || 0;
+
+		return this;
+	}
+
+	toString() {
+		return "[" + this.x + ", " + this.y + "]";
+	}
+}
+
 var version = "0.1";
 
-export { Arc, Circle, MChart, Rectangle, hello, version };
+export { Arc, Circle, MChart, Rectangle, Vector, hello, version };
 //# sourceMappingURL=index.js.map
