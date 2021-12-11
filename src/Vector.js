6bf39f2275b8b2dd410518b4a2770dafd6c7fd18
Marc Mouries <marc@mouries.net>
2021-12-10 22:41:32 -0500

    + copy & sub

--- src/Vector.js
+++ src/Vector.js
@@ -1,14 +1,27 @@
 class Vector {
+	constructor(x, y) {
+		this.x = x || 0;
+		this.y = y || 0;
+	}
 
-  constructor(x, y) {
-    this.x = x || 0;
-    this.y = y || 0;
-  }
-  
-  static add(v1, v2) {
-    return new Vector(v1.x + v2.x, v1.y + v2.y);
-  }
-  
+	static add(v1, v2) {
+		return new Vector(v1.x + v2.x, v1.y + v2.y);
+	}
+
+	copy() {
+		return new Vector(this.x, this.y);
+	}
+
+	sub(v) {
+		this.x -= v.x || 0;
+		this.y -= v.y || 0;
+
+		return this;
+	}
+
+	toString() {
+		return "[" + this.x + ", " + this.y + "]";
+	}
 }
 
 export default Vector;
