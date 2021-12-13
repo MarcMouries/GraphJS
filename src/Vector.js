7a5bb60b6393aab936a0f86e629d23543a0f22d6
Marc Mouries <marc@mouries.net>
2021-12-12 22:25:55 -0500

    + heading

--- src/Vector.js
+++ src/Vector.js
@@ -2,7 +2,7 @@
 2D vector implementation.
 Based on the vector functions in P5.js 
 */
-export class Vector {
+class Vector {
 	constructor(x, y) {
 		this.x = x || 0;
 		this.y = y || 0;
@@ -16,13 +16,38 @@ export class Vector {
 		return result.lerp(v2, amount);
 	}
 
+	copy() {
+		return new Vector(this.x, this.y);
+	}
+
 	lerp(v1, amount) {
 		this.x += (v1.x - this.x) * amount || 0;
 		this.y += (v1.y - this.y) * amount || 0;
 		return this;
 	}
-	copy() {
-		return new Vector(this.x, this.y);
+
+	heading() {
+		const h = Math.atan2(this.y, this.x);
+		return h;
+	}
+
+	magSq() {
+		const x = this.x;
+		const y = this.y;
+		return x * x + y * y;
+	}
+
+	mag() {
+		return Math.sqrt(this.magSq());
+	}
+
+	/**
+	Multiply vector length (ie magnitude) by a constant
+	*/
+	mult(n) {
+		this.x *= n;
+		this.y *= n;
+		return this;
 	}
 
 	sub(v) {
