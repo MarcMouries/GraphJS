2fa4f3a2d2cd4b520e95f87c9d6c761cee786eea
Marc Mouries <marc@mouries.net>
2021-12-20 10:31:32 -0500

    new class

--- src/layout/ForceDirected.js
+++ src/layout/ForceDirected.js
@@ -1,51 +1,71 @@
-import Vector from './Vector';
+import Vector from '../Vector';
 
-export class ForceDirected {
-    constructor(graph) {
-  
-      this.GRAVITY = 2;//0.9;
-      this.REPULSION = 5000;
-  
-    }
-     applyForcesTowardsCenter() {
-      // apply force towards center
-      nodes.forEach((node) => {
-        let gravity = node.pos.copy().mult(-1).mult(this.GRAVITY);
-        node.force = gravity;
-        //node.applyForce(gravity);
-        //console.log(node);
-      });
-    }
-  
-    applyRepulsiveForces() {
-      // apply repulsive force between nodes
-      for (let i = 0; i < nodes.length; i++) {
-        for (let j = i + 1; j < nodes.length; j++) {
-          if (i != j) {
-            let node1 = nodes[i];
-            let node2 = nodes[j];
-            // The gravitational force F between two bodies of mass m1 and m2 is
-            // F = G*m1*m2 / r2
-            // the vector that points from one object to the other
-            // let dir = graphboard.Vector.sub(nodes[j].pos, nodes[i].pos);
-            let dir = graphboard.Vector.sub(node2.pos, node1.pos);
-            // the length (magnitude) of that vector is the distance between the two objects.
-            let distance = dir.mag();
-  
-            //dir.normalize()
-  
-  
-            // The strength of the force is inversely proportional to the distance squared.
-            // The farther away an object is, the weaker the force; the closer, the stronger.
-            let force = dir.div(distance * distance);
-            force.mult(this.REPULSION);
-            console.log(this.REPULSION);
-            node2.force.add(force);
-            let inverseForce = force.copy().mult(-1);
-            node1.force.add(inverseForce);
-          }
+export default class ForceDirected {
+  constructor(graph) {
+
+    this.GRAVITY = 2;//0.9;
+    this.REPULSION = 500000;
+    this.graph = graph;
+
+  }
+  applyForcesTowardsCenter() {
+    // apply force towards center
+    this.graph.nodeList.forEach((node) => {
+      let gravity = node.pos.copy().mult(-1).mult(this.GRAVITY);
+      node.force = gravity;
+      //node.applyForce(gravity);
+      //console.log(node);
+    });
+  }
+
+  applyRepulsiveForces() {
+    // apply repulsive force between nodes
+    for (let i = 0; i < this.graph.nodeList.length; i++) {
+      for (let j = i + 1; j < this.graph.nodeList.length; j++) {
+        if (i != j) {
+          let node1 = this.graph.nodeList[i];
+          let node2 = this.graph.nodeList[j];
+
+          // The gravitational force F between two bodies of mass m1 and m2 is
+          // F = G*m1*m2 / r2
+          // the vector that points from one object to the other
+          let dir = Vector.sub(node2.pos, node1.pos);
+         // let unit = dir.copy().normalize()
+
+          // the length (magnitude) of that vector is the distance between the two objects.
+          let distance = dir.mag();
+
+          // The strength of the force is inversely proportional to the distance squared.
+          // The farther away an object is, the weaker the force; the closer, the stronger.
+
+          // original  : without the normalize
+          dir.normalize()
+
+          let force1 = dir.mult(this.REPULSION);
+          force1.div(distance * distance);
+
+          let inverseForce = force1.copy().mult(-1);
+          node2.force.add(force1);
+          node1.force.add(inverseForce);
         }
       }
     }
-  
   }
+
+  applyForcesExertedByConnections() {
+    this.graph.linkList.forEach((con) => {
+      let node1 = this.graph.nodeList[con[0]];
+      let node2 = this.graph.nodeList[con[1]];
+      
+      //let maxDis = con[2];
+
+      let dir = Vector.sub(node1.pos, node2.pos);
+
+      let neg_force = new Vector(0, 0).sub(dir);
+      let pos_force = new Vector(0, 0).add(dir);
+
+      node1.force.add(neg_force);
+      node2.force.add(pos_force);
+    });
+  }
+}
