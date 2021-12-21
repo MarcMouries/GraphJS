854e762e0395e097101ffcc28737e3d71eea4e53
Marc Mouries <marc@mouries.net>
2021-12-21 15:42:46 -0500

    moved applyForce to Layout

--- src/graph/Node.js
+++ src/graph/Node.js
@@ -1,49 +1,36 @@
+import Vector from "../Vector";
 
-import Vector from '../Vector';
-
-  const PI_2 = Math.PI * 2;
+const PI_2 = Math.PI * 2;
 
 export default class Node {
+	constructor(id) {
+		this.id = id;
+		this.size = 20;
+		this.mass = (PI_2 * this.size) / 1.5;
+		this.radius = this.size;
 
+		this.force = new Vector(0, 0);
 
-    constructor(id) {
-      this.id = id;
-      this.size = 20;
-      this.mass = (PI_2 * this.size) / 1.5;
-      this.radius = this.size;
+		this.pos = new Vector(0, 0);
+		this.velocity = new Vector(0, 0);
+		this.acceleration = new Vector(0, 0);
+	}
 
-      this.pos = new Vector(0, 0);
-      this.force = new Vector(0, 0);
-      this.velocity = new Vector(0, 0);
-      this.acceleration = new Vector(0, 0);
-    }
-    draw(ctx) {
-      ctx.beginPath();
-//      ctx.fillStyle = "darkGrey";
-      ctx.fillStyle = "rgb(176,225,206)";
+	draw(ctx) {
+		ctx.beginPath();
+		//ctx.fillStyle = "darkGrey";
+		ctx.fillStyle = "rgb(176,225,206)";
 
-      ctx.arc(this.pos.x, this.pos.y, this.radius, 0, PI_2, false);
-      ctx.fill();
-      ctx.closePath();
-      ctx.fillStyle = "black";
-      ctx.textAlign = "center";
-      ctx.textBaseline = "middle";
-      ctx.fillText(this.id, this.pos.x, this.pos.y);
-    }
+		ctx.arc(this.pos.x, this.pos.y, this.radius, 0, PI_2, false);
+		ctx.fill();
+		ctx.closePath();
+		ctx.fillStyle = "black";
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+		ctx.fillText(this.id, this.pos.x, this.pos.y);
+	}
 
-    /**
-     *  applyForce
-     *
-     *  Newton’s second law.
-     *  Receive a force, divide by mass, and add to acceleration.
-    */
-    __applyForce(force) {
-      let f = Vector.div(force, this.mass);
-      this.acceleration.add(f);
-    }
-
-
-    toString() {
-      return "[" + this.id + ", " + this.pos.x + ", " + this.pos.y + "]";
-    }
-  }
+	toString() {
+		return "[" + this.id + ", " + this.pos.x + ", " + this.pos.y + "]";
+	}
+}
