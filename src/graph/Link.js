095a080657794b241459d6e0004c209142a672e5
Marc Mouries <marc@mouries.net>
2021-12-21 22:28:42 -0500

    set id with  →

--- src/graph/Link.js
+++ src/graph/Link.js
@@ -1,10 +1,9 @@
 export default class Link {
 	constructor(source, target) {
-		if (source.id & target.id) {
-			this.id = source.id + "-" + target.id;
-		}
-		else {
-			this.id = source + "-" + target;
+		if (source.id && target.id) {
+			this.id = source.id + " → " + target.id;
+		} else {
+			this.id = source + " → " + target;
 		}
 		this.source = source;
 		this.target = target;
