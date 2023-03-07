43bdd3dce9c71ecae8f69d37c8b758d543f74ae6
Marc Mouries <marc@mouries.net>
2023-03-07 08:43:55 -0500

    new

---
+++ src/layout/GraphLayout.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+//import Graph from "../graph/Graph";
+//import Node from "../graph/Node";
+
+
+export default class GraphLayout {
+
+    // @ts-ignore
+    constructor(shapeType, x, y, width, height) {     }
+
+    // @ts-ignore
+    calculate_Positions(graph, starting_vertex, center) {
+        console.error("not implemented")
+    }
+}
