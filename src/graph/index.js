10caa804d5f4c66a0442e082952135a09b95e400
Marc Mouries <marc@mouries.net>
2023-03-08 07:31:58 -0500

    TreeNode

--- src/graph/index.js
+++ src/graph/index.js
@@ -1,3 +1,5 @@
 export {default as Graph} from "./Graph.js";
+export {Tree} from "./Tree.js";
 export {default as Node} from "./Node.js";
+export {TreeNode} from "./TreeNode.js";
 export {default as Link} from "./Link.js";
