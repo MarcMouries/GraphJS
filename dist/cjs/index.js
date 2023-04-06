984222d5921b79c70e1a6e835af50e0900f6a47a
MarcMouries <marc@mouries.net>
2023-04-06 00:10:17 -0400

    new

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -599,19 +599,16 @@ function printNode(node) {
 }
 
 class TreeNode extends Node {
-  constructor(nodeID, nodeData, parentID) {
+  constructor(nodeID, nodeData) {
     super(nodeID, nodeData);
-    this.parentID = parentID;
     this.children = [];
     this.parent;
-    //this.neighbor;
-
-    console.log("TreeNode: constructor: ", nodeID, nodeData, parentID);
+    this.level = 0;
   }
 
   addChild(node) {
-    // const childNode = new TreeNode(nodeID, nodeData, this.nodeID);
     this.children.push(node);
+    node.parent = this;
     return node;
   }
 
@@ -700,6 +697,11 @@ class TreeNode extends Node {
   hasLeftSibling() {
     return !this.isLeftMost();
   }
+
+  getIndex () {
+    return this.parent.children.indexOf(this);
+  }
+
 }
 
 //import Link from "./Link";
@@ -714,7 +716,7 @@ class Tree extends Graph {
   setRoot(nodeID) {
     this.root = nodeID;
   }
-  getRoot () {
+  getRoot() {
     return this.root;
   }
 
@@ -724,10 +726,10 @@ class Tree extends Graph {
 
   traverseDF(callback) {
     function traverse(node) {
-        callback(node);
-        if (node.children) {
-          node.children.forEach(traverse);
-        }
+      callback(node);
+      if (node.children) {
+        node.children.forEach(traverse);
+      }
     }
     traverse(this.root);
   }
@@ -741,47 +743,40 @@ class Tree extends Graph {
     }
   }
 
+  /**
+   * Returns { status: 'success'} or { status: 'error', message: "error message"}
+   * @param {*} json
+   */
   loadFromJSON(json) {
     const data = JSON.parse(json);
-  
+
     // create nodes
     data.nodes.forEach((nodeData) => {
-      const { id, parentId, data } = nodeData;
-      const node = new TreeNode(id, data, parentId);
+      const { id, data } = nodeData;
+      const node = new TreeNode(id, data, null);
       this.nodeMap.set(id, node);
       // add node to nodesByLevel array
       //console.log("")
       //addNodeToLevel(id, parentId, nodesByLevel, node);
     });
-  
+
     // Add child nodes to parent nodes
     data.nodes.forEach((nodeData) => {
       const { id, parentId } = nodeData;
       const node = this.nodeMap.get(id);
       if (parentId) {
         const parent = this.nodeMap.get(parentId);
+        if (!parent) {
+          return { status: "error", message: "Parent node not found for parentId: " + parentId };
+        }
         parent.addChild(node);
+        node.level = node.parent.level + 1;
       } else {
         this.root = node;
       }
     });
-    
-    // Function to add node to nodesByLevel array
-    /*
-    function addNodeToLevel(id, parentId, nodesByLevel, node) {
-
-      const level = parentId ? nodesByLevel[parentId].level + 1 : 0;
-      if (!nodesByLevel[level]) {
-        nodesByLevel[level] = [node];
-      } else {
-        nodesByLevel[level].push(node);
-      }
-      node.level = level;
-    }
-     */
+    return { status: "success" };
   }
-  
-
 }
 
 /* eslint-disable no-unused-vars */
@@ -959,17 +954,45 @@ class ForceDirected extends AbstractGraphLayout {
 	}
 }
 
-class TreeLayout extends AbstractGraphLayout {
+const DEFAULTS = {
+  rootOrientation: "NORTH",
+  maximumDepth: 3,
+  levelSeparation: 50 /* distance between levels = vertical spread */,
+  siblingSpacing: 50 /* distance between leaf siblings */,
+  subtreeSeparation: 160 /* distance between each subtree */,
+  stackedLeaves: true,
+  stackedIndentation : 40,
+  nodeWidth: 0,
+  nodeHeight: 0
+};
 
-  constructor(tree) {
+class TreeLayout extends AbstractGraphLayout {
+  constructor(tree, options) {
     super(tree);
     /**
-		 * lastNodeAtLevel: stores the last node visited at each level to set as left most nodes' neighbor
-		 */
-		this.lastNodeAtLevel = [];
+     * lastNodeAtLevel: stores the last node visited at each level to set as left most nodes' neighbor
+     */
+    this.lastNodeAtLevel = [];
 
+    this.options = Object.assign({}, DEFAULTS, options);
+    options || (options = {});
+    for (let i in DEFAULTS) {
+      if (i in options) {
+        this[i] = options[i];
+      } else {
+        this[i] = DEFAULTS[i];
+      }
+    }
+
+    console.log("TreeLayout constructed.");
+    console.log(this);
+
+    /**
+     * Do a post-order traversal (ie: from the bottom-left to the top-right)
+     * Visit the current node after visiting all the nodes from left to right.
+     */
     const firstWalk = (node, level) => {
-      console.log("firstWalk", node, level);
+      //console.log("firstWalk", node, level);
 
       // private function implementation
       node.prelim = 0;
@@ -981,12 +1004,67 @@ class TreeLayout extends AbstractGraphLayout {
 
       //
       let leftSibling = node.getLeftSibling();
-			console.log("leftSibling  = " + leftSibling);
+      //console.log("leftSibling  = " + leftSibling);
+      if (node.isLeaf() || node.level == this.maximumDepth) {
+        if (leftSibling) {
+          /*-------------------------------------------------
+           * Determine the preliminary x-coordinate based on:
+           * - preliminary x-coordinate of left sibling,
+           * - the separation between sibling nodes, and
+           * - mean width of left sibling & current node.
+           *-------------------------------------------------*/
+          //console.log("\\___ firstWalk Sibling: left=" + leftSibling.id + " right=" + node.id);
+          node.prelim = leftSibling.prelim + this.siblingSpacing;
+          let meanNodeSize = getMeanNodeSize(node, leftSibling);
+          //	console.log("meanNodeSize = " + meanNodeSize);
+          node.prelim += meanNodeSize;
+          //console.log("prelim = " + leftSibling.prelim + " + " + this.siblingSpacing + " + " + meanNodeSize + " = " + node.prelim);
+        } else {
+          /*  no sibling on the left to worry about  */
+          node.prelim = 0;
+          //console.log(node.id + " is a leaf with no left sibling");
+          //console.log("prelim  = " + node.prelim);
+          //console.log("modifier= " + node.modifier);
+        }
+      } else {
+        /* This Node is not a leaf, so call this procedure 
+        /* recursively for each of its offspring.          */
+        var children_count = node.getChildrenCount();
+        for (let i = 0; i < children_count; i++) {
+          let child = node.getAdjacents()[i];
+          firstWalk(child, level + 1);
+        }
+        //console.log(node);
 
+        var midPoint = getMidPoint(node);
+        //console.log("midPoint of " + node.id + "= " + midPoint);
+
+        //console.log(node.id + " is the parent of nodes " + leftMostChild.id + " and " + rightMostChild.id);
+
+        if (leftSibling) {
+          node.prelim += leftSibling.prelim + this.siblingSpacing;
+          let meanNodeSize = getMeanNodeSize(node, leftSibling);
+          node.prelim += meanNodeSize;
+          node.modifier = node.prelim - midPoint;
+          //console.log("prelim = " + leftSibling.prelim + " + " + this.siblingSpacing + " + " + meanNodeSize + " = " + node.prelim);
+          //console.log("modifier= " + node.prelim + " - " + node.modifier);
+          console.log("Calling Apportion for = " + node.id + " - level = " + level);
+          apportion(node, level);
+        } else {
+          node.prelim = midPoint;
+          //console.log("prelim  = " + node.prelim);
+        }
+      }
+    };
+
+    const getMidPoint = (node) => {
+      var leftMostChild = node.getLeftMostChild();
+      var rightMostChild = node.getRightMostChild();
+      var midPoint = (leftMostChild.prelim + rightMostChild.prelim) / 2;
+      return midPoint;
     };
 
     const setNodeNeighbor = (node) => {
-      console.log("setNodeNeighbor", node.toString());
       let isLeftMost = node.isLeftMost();
       let isRightMost = node.isRightMost();
       console.log("setNodeNeighbor NODE= " + node.id + " , level= " + node.level + ", isLeftMost(" + isLeftMost + ")" + ", isRightMost(" + isRightMost + ")");
@@ -994,17 +1072,194 @@ class TreeLayout extends AbstractGraphLayout {
         //console.log("\\_setNodeNeighbor lastNodeAtLevel      = " + node.id);
         //console.log("\\_setNodeNeighbor this.lastNodeAtLevel[node.level]       = " + node);
         this.lastNodeAtLevel[node.level] = node;
-      }
-      else if (isLeftMost) {
+      } else if (isLeftMost) {
         node.neighbor = this.lastNodeAtLevel[node.level];
         if (node.neighbor) ;
-      }
-      else ;
+      } else ;
     };
+
+    const getMeanNodeSize = (leftNode, rightNode) => {
+      var meanNodeSize = 0.0;
+      switch (this.rootOrientation) {
+        case "NORTH":
+        case "SOUTH":
+          if (leftNode) {
+            meanNodeSize = leftNode.width; /// 2;
+          }
+          if (rightNode) {
+            meanNodeSize = rightNode.width; // / 2;
+          }
+          break;
+        case "EAST":
+        case "WEST":
+          if (leftNode) {
+            meanNodeSize = leftNode.height / 2;
+          }
+          if (rightNode) {
+            meanNodeSize = rightNode.height / 2;
+          }
+          break;
+      }
+      return meanNodeSize;
+    };
+
+    /**
+     * Determine the leftmost descendant of a node at a given depth.
+     * This is implemented using a post-order walk of the subtree
+     * under node, down to the level of searchDepth.
+     * If we've searched to the proper distance, return the currently leftmost node.
+     * Otherwise, recursively look at the progressively lower levels.
+     */
+    const getLeftmost = (node, currentLevel, searchDepth) => {
+      //console.log("START getLeftmost= " + node.id + "/" + currentLevel + "/" + searchDepth);
+
+      /*  searched far enough.           */
+      if (currentLevel >= searchDepth) {
+        return node;
+      } else if (node.isLeaf()) {
+        return null; /* This node has no descendants    */
+      } else {
+        /* Do a post-order walk of the subtree.     */
+        var children_count = node.getChildrenCount();
+        //console.log("  " + ThisNode.id + "/  children_count=" + children_count);
+        for (var i = 0; i < children_count; i++) {
+          let child = node.children[i];
+          let leftmost = getLeftmost(child, currentLevel + 1, searchDepth);
+          if (leftmost) {
+            return leftmost;
+          }
+        }
+      }
+    };
+
+    /*------------------------------------------------------
+     * Clean up the positioning of small sibling subtrees.
+     * Subtrees of a node are formed independently and placed as close together as possible.
+     * By requiring that the subtrees be rigid at the time they are put together, we avoid
+     * the undesirable effects that can accrue from positioning nodes rather than subtrees.
+     *
+     *  Called for non-leaf nodes
+     *----------------------------------------------------*/
+    const apportion = (node, level) => {
+      //console.log("_apportion " + node.id);
+
+      var firstChild = node.children[0];
+      var firstChildLeftNeighbor = firstChild.neighbor;
+      var compareDepth = 1;
+      var depthToStop = this.maximumDepth - level;
+
+      if (firstChild && firstChildLeftNeighbor && compareDepth < depthToStop) {
+        var rightModSum, leftModSum, rightAncestor, leftAncestor;
+
+        leftModSum = 0;
+        rightModSum = 0;
+        rightAncestor = firstChild;
+        leftAncestor = firstChildLeftNeighbor;
+        for (var l = 0; l < compareDepth; l += 1) {
+          rightAncestor = rightAncestor.parent;
+          leftAncestor = leftAncestor.parent;
+          rightModSum += rightAncestor.modifier;
+          leftModSum += leftAncestor.modifier;
+        }
+        /**
+         * Find the moveDistance, and apply it to Node's subtree.
+         * Apply appropriate portions to smaller interior subtrees.
+         **/
+        var meanNodeSize = 10; //firstChildLeftNeighbor._getSize(this.orientation);
+
+        var totalGap = firstChildLeftNeighbor.prelim + leftModSum + this.subtreeSeparation + meanNodeSize - (firstChild.prelim + rightModSum);
+        //console.log("\\__apportion: totalGap of " + node.id + " = " + totalGap);
+
+        if (totalGap > 0) {
+          /* Count interior sibling subtrees in LeftSiblings */
+
+          var subtree, subtreeMoveAux;
+
+          var numberOfLeftSiblings = 0;
+          for (subtree = node; subtree && subtree !== leftAncestor; subtree = subtree.getLeftSibling()) {
+            numberOfLeftSiblings += 1;
+            //console.log("\\__apportion: numberOfLeftSiblings: " + numberOfLeftSiblings);
+            //console.log("\\__apportion: leftAncestor = " + leftAncestor.id);
+          }
+
+          if (subtree) {
+            /* Apply portions to appropriate leftsibling subtrees. */
+            var portion = totalGap / numberOfLeftSiblings;
+            subtreeMoveAux = node;
+
+            while (subtreeMoveAux !== leftAncestor) {
+              //console.log("\\__apportion: subtree " + subtree.id + " & " + "subtreeMoveAux " + subtreeMoveAux.id);
+
+              subtreeMoveAux.prelim += totalGap;
+              subtreeMoveAux.modifier += totalGap;
+              totalGap -= portion;
+              subtreeMoveAux = subtreeMoveAux.getLeftSibling();
+            }
+          } else {
+            /* Don't need to move anything--it needs to be done by an ancestor because      */
+            /* pAncestorNeighbor and pAncestorLeftmost are not siblings of each other.      */
+            return;
+          }
+        } /* end of the while  */
+
+        /* Determine the leftmost descendant of thisNode */
+        /* at the next lower level to compare its         */
+        /* positioning against that of its neighbor.     */
+        compareDepth++;
+
+        if (firstChild.getChildrenCount() === 0) {
+          firstChild = getLeftmost(node, 0, compareDepth);
+        } else {
+          firstChild = firstChild.getFirstChild();
+        }
+        if (firstChild) {
+          firstChildLeftNeighbor = firstChild.neighbor;
+        }
+      }
+
+
+    }; // apportion
+
+      /*------------------------------------------------------
+       * During a second pre-order walk, each node is given a final x-coordinate by summing its preliminary
+       * x-coordinate and the modifiers of all the node's ancestors.
+       * The y-coordinate depends on the height of the tree.
+       * (The roles of x and y are reversed for RootOrientations of EAST or WEST.)
+       * Returns: TRUE if no errors, otherwise returns FALSE.
+       *----------------------------------------- ----------*/
+      const secondWalk = (node, level, modSum) => {
+        //console.log("secondWalk    = " + node);
+        if (level <= this.maximumDepth) {
+          var xTopAdjustment = 0;
+          var yTopAdjustment = 0;
+
+          node.x = xTopAdjustment + node.prelim + modSum;
+          node.y = yTopAdjustment + level * this.levelSeparation;
+          //console.log("\\secondWalk: Node(" + node.id + " / " + xTopAdjustment + " / " + node.prelim + " / " + modSum);
+          //console.log("\\secondWalk: " + node.x + "," + node.y);
+
+          if (this.leavesStacked) {
+            if (node.isLeaf()) {
+              const indentation = 30;
+              let index = node.getIndex();
+              node.x = node.parent.x + indentation;
+              node.y += node.getIndex() * this.nodeHeight + node.getIndex() * this.siblingSpacing; //	shift the node down
+              console.log(`secondWalk: ${node} #${index}  (${node.x}, ${node.y})`);
+            }
+          }
+
+
+          var children_count = node.getChildrenCount();
+          for (var i = 0; i < children_count; i++) {
+            var child = node.children[i];
+            secondWalk(child, level + 1, modSum + node.modifier);
+          }
+        }
+      };
+
 
     // PUBLIC FUNCTIONS
     this.calculate_Positions = (root, center) => {
-
       console.log("calculate_Positions", this, center);
       //var root = this.graph.getRoot();
       console.log("root", root);
@@ -1012,8 +1267,11 @@ class TreeLayout extends AbstractGraphLayout {
 
       // call the private function
       firstWalk(starting_node, 0);
+      secondWalk(starting_node, 0, 0);
+    };
 
-      // public function implementation
+    this.getTreeDimension = () => {
+        return {};
     };
   }
 }
