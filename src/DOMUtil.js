fc981240bdb4a1ed16f96aad37e0a0821e08673f
MarcMouries <marc@mouries.net>
2023-04-24 09:20:03 -0400

    accept string or element

--- src/DOMUtil.js
+++ src/DOMUtil.js
@@ -1,22 +1,25 @@
 export class DOMUtil {
 
   /** calculate the dimensions of the specified DOM element */
-  static getDimensions(elementHTML) {
+  static getDimensions(element) {
 
-    // Create a temporary DOM element to calculate the dimensions
-    const tempElement = document.createElement('div');
-    tempElement.style.position = 'absolute';
-    tempElement.style.visibility = 'hidden';
-    tempElement.innerHTML = elementHTML;
-    document.body.appendChild(tempElement);
+    // Check if the input is an HTML string or a DOM element
+    if (typeof element === 'string') {
+      const tempElement = document.createElement('div');
+      tempElement.innerHTML = element;
+      element = tempElement.firstChild;
+    }
 
+    // Clone the element to avoid side effects when adding to the document
+    const clonedElement = element.cloneNode(true);
+
+    // Add the cloned element to the body to calculate the dimensions
+    document.body.appendChild(clonedElement);
     const dimensions = {
-      width: tempElement.offsetWidth,
-      height: tempElement.offsetHeight,
+      width: clonedElement.offsetWidth,
+      height: clonedElement.offsetHeight,
     };
-
-    document.body.removeChild(tempElement);
-
+    document.body.removeChild(clonedElement);
     return dimensions;
   }
 }
