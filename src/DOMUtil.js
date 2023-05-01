97ccb75de0b1d05d02b9cbfa4a60784596eae11a
MarcMouries <marc@mouries.net>
2023-04-30 22:51:03 -0400

    position = "absolute"

--- src/DOMUtil.js
+++ src/DOMUtil.js
@@ -3,23 +3,17 @@ export class DOMUtil {
   /** calculate the dimensions of the specified DOM element */
   static getDimensions(element) {
 
-    // Check if the input is an HTML string or a DOM element
-    if (typeof element === 'string') {
-      const tempElement = document.createElement('div');
-      tempElement.innerHTML = element;
-      element = tempElement.firstChild;
-    }
+    const tempElement = document.createElement("div");
+    tempElement.style.position = "absolute";
+    tempElement.style.visibility = "hidden";
+    tempElement.appendChild(element);
+    document.body.appendChild(tempElement);
 
-    // Clone the element to avoid side effects when adding to the document
-    const clonedElement = element.cloneNode(true);
+    const width = tempElement.offsetWidth;
+    const height = tempElement.offsetHeight;
 
-    // Add the cloned element to the body to calculate the dimensions
-    document.body.appendChild(clonedElement);
-    const dimensions = {
-      width: clonedElement.offsetWidth,
-      height: clonedElement.offsetHeight,
-    };
-    document.body.removeChild(clonedElement);
-    return dimensions;
+    document.body.removeChild(tempElement);
+
+    return { width, height };
   }
 }
