7937e376937fcfb16d9e3910854a704597fb77fd
MarcMouries <marc@mouries.net>
2023-05-07 14:15:34 -0400

    getDimensions + margin + border + padding

--- src/DOMUtil.js
+++ src/DOMUtil.js
@@ -3,14 +3,37 @@ export class DOMUtil {
   /** calculate the dimensions of the specified DOM element */
   static getDimensions(element) {
 
+   // element.setAttribute("style", "border: 10px solid black; box-shadow: 2px 2px 5px #888;");
+
+
     const tempElement = document.createElement("div");
     tempElement.style.position = "absolute";
     tempElement.style.visibility = "hidden";
     tempElement.appendChild(element);
+
     document.body.appendChild(tempElement);
 
-    const width = tempElement.offsetWidth;
-    const height = tempElement.offsetHeight;
+    const style = window.getComputedStyle(element);
+    console.log("style", style)
+
+    const height = element.offsetHeight +
+    parseInt(style.marginTop) +
+    parseInt(style.marginBottom) +
+    parseInt(style.borderTopWidth) +
+    parseInt(style.borderBottomWidth) +
+    parseInt(style.paddingTop) +
+    parseInt(style.paddingBottom);
+
+  const width = element.offsetWidth +
+    parseInt(style.marginLeft) +
+    parseInt(style.marginRight) +
+    parseInt(style.borderLeftWidth) +
+    parseInt(style.borderRightWidth) +
+    parseInt(style.paddingLeft) +
+    parseInt(style.paddingRight);
+
+//    const width = tempElement.offsetWidth;
+//    const height = tempElement.offsetHeight;
 
     document.body.removeChild(tempElement);
 
